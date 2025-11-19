ee6198fc77448744f8f8d4115d75a219882ea3ba
Alessandro Junior <alessandrojunior466@gmail.com>
2025-11-19 14:59:20 -0300

    19/11

--- js/script.js
+++ js/script.js
@@ -1,23 +1,2 @@
-let indiceAtual = 0;
-  const faixa = document.getElementById('faixa');
-
-  function atualizarCarrossel() {
-    const larguraImagem = faixa.children[0].clientWidth;
-    faixa.style.transform = `translateX(-${indiceAtual * larguraImagem}px)`;
-  }
-
-  function mudarImagem(direcao) {
-    const totalImagens = faixa.children.length;
-    indiceAtual += direcao;
-
-    if (indiceAtual < 0) {
-      indiceAtual = totalImagens - 1;
-    } else if (indiceAtual >= totalImagens) {
-      indiceAtual = 0;
-    }
-
-    atualizarCarrossel();
-  }
 
 
-  window.addEventListener('resize', atualizarCarrossel);
